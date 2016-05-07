2312b194eed7c0909c08683823a578bd4e084a1b
Matt Smith <runner_28@hotmail.com>
2016-05-07 18:59:28 -0400

    Added test 🏓

---
+++ tests/js/ipsum.js
@@ -0,0 +1,40 @@
+function loadJSON(path, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        if (success) {
+          success(JSON.parse(xhr.responseText));
+        }
+      } else {
+        if (error) {
+          error(xhr);
+        }
+      }
+    }
+  };
+  xhr.open("GET", path, true);
+  xhr.send();
+}
+
+function generateIpsum(e) {
+  loadJSON('data.json',
+     function (data) { console.log(data); },
+     function (xhr) { console.error(xhr); }
+    );
+  
+  var minCount = 5,
+    maxCount = 11,
+    ipsum = Math.floor(Math.random() * (maxCount - minCount)) + minCount,
+    ret = '';
+  
+  for (i = 0; i < ipsum; i++) {
+    var newIpsum = quotes[Math.floor(Math.random() * (quotes.length - 1))];
+    if (ret.substring(ret.length - 1, ret.length) === "." || ret.substring(ret.length - 1, ret.length) === "?") {
+      newIpsum = newIpsum.substring(0, 1).toUpperCase() + newIpsum.substring(1, newIpsum.length);
+    }
+    ret += ' ' + newIpsum;
+  }
+    
+  document.getElementById(e).innerHTML = document.getElementById(e).innerHTML + '<p>' + ret.substring(0, ret.length + 1) + '</p>';
+}
